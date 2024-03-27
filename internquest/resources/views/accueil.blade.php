3d88bfa8c19807dec249e298cd181aad5d203259
sadastral <eliote.sadaoui@viacesi.fr>
2024-03-27 12:17:17 +0100

    front login signin

--- internquest/resources/views/accueil.blade.php
+++ internquest/resources/views/accueil.blade.php
@@ -41,9 +41,9 @@
                     </form>
                     @endauth
                     @guest
-                        <div class="text-gray-700 hidden md:flex space-x-16">
-                            <a href="{{route('auth.login')}}" class="py-2 px-3 bg-gray-200 text-gray-700 rounded-3xl hover:bg-gray-300 transition duration-300"> se connecter</a>
-                            <a href="{{ route('users.create') }}" class="py-2 px-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300" onclick="toggleDialog(), preventReload(event)">S'inscrire</a>
+                        <div class="text-gray-700 items-center hidden md:flex space-x-8">
+                            <a href="#" class="py-2 px-3 bg-gray-200 text-gray-700 rounded-3xl hover:bg-gray-300 transition duration-300" onclick="login(), preventReload(event)" > se connecter</a>
+                            <a href="#" class="py-2 px-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300" onclick="signin(), preventReload(event)">S'inscrire</a>
                         </div>
                     @endguest
                 </div>
@@ -51,6 +51,7 @@
         </div>
     </div>
 </nav>
+<div class="w-full h-screen bg-no-repeat bg-cover" style="background-image: url('/img/image-bg.jpg')" > </div>
 <div class="container">
     @if (session('success'))
         <div class="alert alert-success">
@@ -75,20 +76,26 @@
             </div>
         }
         @endif
-        <dialog id="signin_dialog" class="fixed inset-0 m-auto w-100 h-100 bg-transparent border-2 border-white border-opacity-50 rounded-3xl shadow-2xl flex items-center justify-center overflow-hidden" style="backdrop-filter: blur(20px); display: none;" open>
-            <div class="page-1">
-                <form action="{{route('users.store')}}" method="POST">
+        <dialog id="signin_dialog" class="fixed inset-0 m-auto w-100 h-110 bg-transparent border-2 border-white border-opacity-50 rounded-3xl shadow-2xl flex items-center justify-center overflow-hidden" style="backdrop-filter: blur(20px); display: none;" open>
+            <div class="w-full p-10 flex flex-col items-center">
+            <button class="absolute top-0 right-0 mt-4 mr-4 bg-gray-300 text-gray-700 hover:bg-gray-400 rounded-2xl p-2 focus:outline-none" onclick="closesignin(), preventReload(event)">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+            </button>
+            <h2 class="text-4xl text-blue-600 mb-6">S'inscrire</h2>
+                <form action="{{route('users.store')}}" method="POST" class = w-full>
                     @csrf
                     <div class="relative mb-6">
                         <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">Nom</label>
-                        <input type="text" name="nom" id="nom" required placeholder="Nom" class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                        <input type="text" name="nom" id="nom" required placeholder="Nom" class="w-full pl-7 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
                         @error('nom')
                         <p style="color: red;">{{$message}}</p>
                         @enderror
                     </div>
                     <div class="relative mb-6">
                         <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">Prenom</label>
-                        <input type="text" name="prenom" id="prenom" required placeholder="Prenom" class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                        <input type="text" name="prenom" id="prenom" required placeholder="Prenom" class="w-full pl-7 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
                         @error('prenom')
                         <p style="color: red;">{{$message}}</p>
                         @enderror
@@ -96,7 +103,7 @@
                     <div class="relative mb-6">
                         <ion-icon name="mail" class="absolute text-gray-700 text-lg left-2 top-1/2 transform -translate-y-1/2"></ion-icon>
                         <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">Email</label>
-                        <input type="email" name="email" id="email" required placeholder="email@example.fr" class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                        <input type="email" name="email" id="email" required placeholder="email@example.fr" class="w-full pl-7 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
                         @error('email')
                         <p style="color: red;">{{$message}}</p>
                         @enderror
@@ -104,7 +111,7 @@
                     <div class="relative mb-6">
                         <ion-icon name="lock-closed" class="absolute text-gray-700 text-lg left-2 top-1/2 transform -translate-y-1/2"></ion-icon>
                         <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">Mot de passe</label>
-                        <input type="password" name="password" id="password" required placeholder="password" class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                        <input type="password" name="password" id="password" required placeholder="password" class="w-full pl-7 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
                         <span>
                             @error('password')
                             <p style="color: red;">{{$message}}</p>
@@ -113,13 +120,14 @@
                     </div>
                     <div class="relative mb-6">
                         <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">Identifiant</label>
-                        <input type="text" name="username" id="username" required placeholder="Identifiant" class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                        <input type="text" name="username" id="username" required placeholder="Identifiant" class="w-full pl-7 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
                         @error('username')
                         <p style="color: red;">{{$message}}</p>
                         @enderror
+                    </div>
                         <div class="relative mb-6">
-                            <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">Role</label>
-                            <select name="role" id="role" required class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                            <label class="absolute left-2 -top-6 text-base text-gray-700 font-medium transition-all">Role</label>
+                            <select name="role" id="role" required class="w-full pl-7 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
                                 <option value="Admin">Admin</option>
                                 <option value="Pilote">Pilote</option>
                                 <option value="Etudiant">Etudiant</option>
@@ -129,34 +137,91 @@
                             @enderror
                         </div>
                     <div class="relative mb-6">
-                        <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">Centre</label>
-                        <input type="text" name="centre" id="centre" required placeholder="Centre" class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                        <label class="absolute left-2 -top-6 text-base text-gray-700 font-medium transition-all">Centre</label>
+                        <input type="text" name="centre" id="centre" required placeholder="Centre" class="w-full pl-7 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
                         @error('centre')
                         <p style="color: red;">{{$message}}</p>
                         @enderror
                     </div>
+                    <div class="flex justify-between items-center mb-4">
+                <label class="flex items-center text-base text-gray-700 font-medium">
+                    <input type="checkbox" class="form-checkbox h-5 w-5 text-blue-600 mr-2">se rappeler de moi
+                </label>
+                <a href="#" class="text-sm text-gray-700 hover:underline mx-3">Politique de confidentialité</a>
+                </div>
                     <div>
-                        <<button type="submit" class="w-full h-11 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 font-medium">S'enregistrer</button>
+                        <button type="submit" class="w-full h-11 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 font-medium">Soumettre la demande</button>
                     </div>
                 </form>
             </div>
         </dialog>
+        <dialog id="login_dialog" class="fixed inset-0 m-auto w-100 h-100 bg-transparent border-2 border-white border-opacity-50 rounded-3xl shadow-2xl flex items-center justify-center overflow-hidden" style="backdrop-filter: blur(20px); display: none;" open>
+        <div class="w-full p-10 flex flex-col items-center">
+        <button class="absolute top-0 right-0 mt-4 mr-4 bg-gray-300 text-gray-700 hover:bg-gray-400 rounded-2xl p-2 focus:outline-none" onclick="closelogin(), preventReload(event)">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+            </button>
+        <h2 class="text-4xl text-blue-600 mb-6">Se connecter</h2>
+        <form action="{{route('auth.login')}}" class="w-full" method="POST">
+            @csrf
+            <div class="relative mb-6">
+                <ion-icon name="mail" class="absolute text-gray-700 text-lg left-2 top-1/2 transform -translate-y-1/2"></ion-icon>
+                <input type="email" name="email" id="email" required placeholder="" class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">email</label>
+                @error('email')
+                <p style="color: red">{{$message}}</p>
+            @enderror
+            </div>
+            <div class="relative mb-6">
+                <ion-icon name="lock-closed" class="absolute text-gray-700 text-lg left-2 top-1/2 transform -translate-y-1/2"></ion-icon>
+                <input type="password" name="password" id="password" required placeholder=" " class="w-full pl-10 pr-3 py-1 bg-transparent border-b-2 border-blue-600 outline-none focus:border-blue-400">
+                <label class="absolute left-2 -top-4 text-base text-gray-700 font-medium transition-all">mot de passe</label>
+                @error('password')
+                <p style="color: red;">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="flex justify-between items-center mb-4">
+                <label class="flex items-center text-base text-gray-700 font-medium">
+                    <input type="checkbox" class="form-checkbox h-5 w-5 text-blue-600 mr-2">se rappeler de moi
+                </label>
+                <a href="#" class="text-sm text-gray-700 hover:underline mx-3">mot de passe oublié</a>
+            </div>
+            <button type="submit" class="w-full h-11 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 font-medium">Login</button>
+            <div class="text-center text-sm text-gray-700 mt-4">
+                je n'ai pas de compte <a href="{{route('users.create')}}" class="font-bold hover:underline">s'inscrire</a>
+            </div> 
+        </form>
     </div>
+    </dialog>
 <script>
-    function toggleDialog() {
-        var dialog = document.getElementById('signin_dialog');
-        var nav = document.getElementById('nav_bar');
-
-        if (dialog.style.display === 'none' || dialog.style.display === '') {
-            dialog.style.display = 'block';
-            nav.style.display = 'none';
-        } else {
-            dialog.style.display = 'none';
-        }
-    }
-    function preventReload(event) {
-                event.preventDefault();
-    }
+function login(){
+    console.log("close signin");
+    const signin_dialog = document.getElementById("signin_dialog");
+    signin_dialog.style.display = "none";
+    console.log("Open login");
+    const login_dialog = document.getElementById("login_dialog");
+    login_dialog.style.display = "flex";
+    
+};
+function signin(){
+    console.log("close login");
+    const login_dialog = document.getElementById("login_dialog");
+    login_dialog.style.display = "none";
+    console.log("Open signin");
+    const signin_dialog = document.getElementById("signin_dialog");
+    signin_dialog.style.display = "flex";
+};
+function closelogin(){
+    console.log("close login");
+    const login_dialog = document.getElementById("login_dialog");
+    login_dialog.style.display = "none";
+};
+function closesignin(){
+    console.log("close signin");
+    const signin_dialog = document.getElementById("signin_dialog");
+    signin_dialog.style.display = "none";
+};
 </script>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
