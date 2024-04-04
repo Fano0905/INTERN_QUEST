6ed893d0e1fc3ea703d220c8d4346eac25fa52a5
Tayrac-Thomas <152963945+Tayrac-Thomas@users.noreply.github.com>
2024-04-04 17:00:28 +0200

    04/04
    ajout auth front

--- internquest/resources/views/auth/user.blade.php
+++ internquest/resources/views/auth/user.blade.php
@@ -1,28 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
-    <title>User info</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title', 'Accueil')</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
 
-    @auth
-        <img src="" alt="pdp" height="50" width="50">
-        <p>Last Name: {{Auth::user()->lname}}</p>
-        <p>First Name: {{Auth::user()->fname}}</p>
-        <p>Mail: {{Auth::user()->mail}}</p>
-        <p>You are connected as: {{Auth::user()->role}}</p>
-        <div class="control" style="display: flex; flex-direction:row;">
-            <a href="{{route('users.edit', Auth::user()->id)}}"><button type="submit" class="w-full h-11 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 font-medium">Modifier</button></a>
+
+<body style="height: 200dvh">
+<div class="w-full h-full bg-no-repeat bg-cover overflow-hidden" style="background-image: url('/img/image-bg.jpg'); background-position: left bottom;">
+    <div class="flex justify-center items-center h-screen">
+    <div class="w-256 h-160 border border-black bg-gray-200 rounded-3xl"> 
+        <div class="flex">
+            <div class="w-3/4 p-4">
+
+                <p class="text-lg text-black-700 font-medium transition-all">
+                    Nom : {{ Auth::user()->lname }}
+                </p>
+                <div class="block w-full h-0.5 bg-blue-500 bottom-0 left-0"> </div>
+                
+                <p class="text-lg text-black-700 font-medium transition-all">
+                    Prénom : {{Auth::user()->fname}}
+                </p>
+                <div class="block w-full h-0.5 bg-blue-500 bottom-0 left-0"> </div>
+
+                <p class="text-lg text-black-700 font-medium transition-all">
+                    Rôle : {{Auth::user()->role}}
+                </p>
+                <div class="block w-full h-0.5 bg-blue-500 bottom-0 left-0"> </div>
+
+                <p class="text-lg text-black-700 font-medium transition-all">
+                    Mail : {{Auth::user()->mail}}
+                </p>
+                <div class="block w-full h-0.5 bg-blue-500 bottom-0 left-0"> </div>
+
+            </div>
+
+            <div class="w-1/4 p-4 border photo-container">
+                <img src="img/pdp.png" alt="photo de profil utilisateur" class="w-3/4 h-3/4">
+            </div>
+        </div>
+
+            <div class="control flex justify-center items-center mt-4">
+            <a href="{{route('users.edit', Auth::user()->id)}}" class="mx-2">
+                <button type="submit" class="w-36 h-12 bg-blue-800 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 font-medium"> <!-- Modification de la classe 'bg-blue-600' en 'bg-blue-800' -->
+                    Modifier
+                </button>
+            </a>
             <form action="{{ route('users.destroy', Auth::user()->id) }}" method="post">
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="w-full h-11 bg-red-600 text-white rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 font-medium">Delete account</button>
+                <button type="submit" class="w-36 h-12 bg-black text-white rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 font-medium">
+                    Supprimer le compte
+                </button>
             </form>
         </div>
-    @endauth
+    </div>
+</div>
+
 </body>
 </html>
