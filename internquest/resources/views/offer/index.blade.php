e16925c54dfd7940758658d39d3da6a9f0fa7b64
eliote.sa <150005933+sadastral@users.noreply.github.com>
2024-04-04 21:32:30 +0200

    Update index.blade.php

--- internquest/resources/views/offer/index.blade.php
+++ internquest/resources/views/offer/index.blade.php
@@ -4,26 +4,6 @@
 
 @section('content')
 
-<<<<<<< Updated upstream
-    @auth
-    @if (Auth::user()->role == 'Admin' || Auth::user()->role == 'Pilote')
-        <a href="{{route('offers.create')}}"><button type="submit" class="w-full h-11 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 font-medium"><ion-icon name="add-outline"></ion-icon>Publier</button></a>
-    @endif
-    @endauth
-    @foreach ($offers as $offer)
-        <strong><h1>{{$offer->title}}</h1></strong>
-        <p>Localité: {{$offer->city}}</p>
-        <p>Durée: {{$offer->duration}}</p>
-        <p>Date de début: {{$offer->date_offer}}</p>
-        <p>Nombre de places: {{$offer->place_offered}}</p>
-        <p style="color: red;">{{$offer->nb_application}} ont postulé à cette offre</p>
-        <form action="{{route('offers.show', $offer->id)}}" method="GET">
-            @csrf
-            <button type="submit" class="w-full h-11 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 font-medium">En savoir plus...</button>
-        </form>
-    @endforeach
-@endsection()
-=======
 <div class="flex justify-center">
     <div class="w-full max-w-4xl">
         @auth
@@ -57,4 +37,3 @@
 </div>
 
 @endsection
->>>>>>> Stashed changes
